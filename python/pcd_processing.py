e43063dd48f192efd65b1a509b9cd6f7e9aa4273
Cem C. Tutum <cem.celal@gmail.com>
2024-10-11 12:43:38 -0500

    loading the point cloud fom local directory

--- python/pcd_processing.py
+++ python/pcd_processing.py
@@ -8,31 +8,26 @@ URL: https://towardsdatascience.com/discover-3d-point-cloud-processing-with-pyth
 
 import numpy as np
 import os
-import requests
 
 #%%
 
 # Ensure the 'data' directory exists
-data_path = "../data"
-os.makedirs(data_path, exist_ok=True)
-
-url = "https://drive.google.com/drive/folders/1Ih_Zz9a6UcbUlaA-puEB_is7DYvXrb4w?usp=sharing"
+directory = "../data"
+os.makedirs(directory, exist_ok=True)
 filename = "sample.xyz"
+file_path = os.path.join(directory, filename)
 
-if filename not in os.listdir(data_path):
-    response = requests.get(f"{url}/{filename}")
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Open the file in write binary mode
-        with open(f"data/{filename}", 'w', encoding='utf-8') as file:
-            file.write(response.text)
-        print(f"File downloaded successfully: data/{filename}")
-    else:
-        print(f"Failed to download {filename}. Status code: {response.status_code}")
-    
-#%%
-    
-file_path = "../data/sample.xyz"
+# NOTE:
+# Manually download "sample.xyz" from the publicly shared Google-Drive-Folder:
+# https://drive.google.com/drive/folders/1Ih_Zz9a6UcbUlaA-puEB_is7DYvXrb4w
+# Automation of this task requires the use of Google Drive API, because parsing
+# the folder's HTML directly using "beautifulsoup4" module does not work.
+
+if not os.path.isfile(file_path):
+    raise FileNotFoundError(f"The file '{filename}' does not exist in the directory '{directory}'")
+else:
+    print(f"The file '{filename}' was found in the directory '{directory}'")    
+ 
 pcd = np.loadtxt(file_path, skiprows=1, max_rows=1_000_000) # X Y Z R G B
 # original row size = 1_505_010
 
