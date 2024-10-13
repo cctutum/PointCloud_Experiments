447ed93553e8e6d05e9539653d379d69d90c8b17
Cem C. Tutum <cem.celal@gmail.com>
2024-10-12 21:19:17 -0500

    added random subsampling

--- python/automate_pcd_subsampling.py
+++ python/automate_pcd_subsampling.py
@@ -8,9 +8,36 @@ the random, the minimal distance and the grid (or uniform) methods.
 
 #%%
 
-import numpy as np
 import os
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+import numpy as np
+import laspy as lp
 
-#%%
+#%% Read point cloud data
+
+directory = "../data"
+os.makedirs(directory, exist_ok=True)
+filename = "NZ19_Wellington.las"
+file_path = os.path.join(directory, filename)
+
+pcd = lp.read(file_path)
+
+#%% Separate points and colors
+
+points = np.vstack((pcd.x, pcd.y, pcd.z)).transpose() # shape: (13993118, 3)
+colors = np.vstack((pcd.red, pcd.green, pcd.blue)).transpose()
+
+#%% Strategy 1: Point Cloud Random subsampling
+# Select one in every 'factor' rows (points)
+
+factor = 160
+decimated_points_random = points[::factor] # [start:end:step] shape: (87457, 3)
+
+#%% Strategy 2: Point Cloud Grid Subsampling
+# The grid subsampling strategy will be based on the division of the 3D space 
+# in regular voxels. For each cell of this grid, we will only keep one representative 
+# point. For example, it can be the barycenter of the points in that cell, or 
+# the closest point to it.
+
+
+
+
