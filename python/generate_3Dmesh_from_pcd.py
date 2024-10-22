e3fd62d681623dc26bfaaac3bcc071856f4dd8ee
Cem C. Tutum <cem.celal@gmail.com>
2024-10-21 23:06:42 -0500

    read the point cloud as a numpy array and converted to open3d.geometry.PointCloud data type

--- python/generate_3Dmesh_from_pcd.py
+++ python/generate_3Dmesh_from_pcd.py
@@ -9,7 +9,33 @@ Tutorial to generate 3D meshes (.obj, .ply, .stl, .gltf) automatically from
 
 #%%
 
+import os
 import numpy as np
 import open3d as o3d
 
-#%%
+#%% Step-1: Read point cloud data
+
+directory = "../data"
+results_dir = "../results"
+os.makedirs(directory, exist_ok=True)
+os.makedirs(results_dir, exist_ok=True)
+filename = "sample_w_normals.xyz"
+file_path = os.path.join(directory, filename)
+
+point_cloud = np.loadtxt(file_path, skiprows= 1) 
+# point_cloud.shape = (1505010, 9)
+
+xyz = point_cloud[:, :3]
+rgb = point_cloud[:, 3:6] / 255
+normals = point_cloud[:, 6:]
+
+# Transform the point_cloud variable type from Numpy to 
+# Open3D o3d.geometry.PointCloud type for further processing
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(xyz)
+pcd.colors = o3d.utility.Vector3dVector(rgb)
+pcd.normals = o3d.utility.Vector3dVector(normals)
+
+o3d.visualization.draw_geometries([pcd])
+
+#%% 
